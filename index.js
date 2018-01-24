31b86412ecb5ed20ab2fa93c3176afa2e8d0d585
Lauren M Hale <lmhale710@gmail.com>
2018-01-24 23:08:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ object.prop2 = 2;
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
     var object = { prop: 1 };
     object.prop = 1;
-    object.prop1 = 2;
+    object.prop2 = 2;
     return object;
 
 }
